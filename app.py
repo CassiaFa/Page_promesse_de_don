055239b9375e1cbe612b5fe7d7f99c759cdf6854
CassiaFa <fabio.gavancha-cassiano@isen-ouest.yncrea.fr>
2022-01-17 20:51:17 +0100

    Communication site -> server python -> bdd
    Ajout du fichier add.html, necessite un relooking ou remplacer par un pop-up javascript
    
    Ajout dans data.py des fontions permettant l'ajout des dons dans la bdd.
    -> bdd à retravailler
    --> ajout des données à modifier en fonction

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import data
 
 app = Flask(__name__)
 
@@ -14,9 +15,27 @@ def don():
 def contact():
     return render_template('contact.html')
 
-# @app.route('/bonjour/<name>')
-# def bonjour(name):
-#     return render_template('bonjour.html', quelquun=name)
+@app.route('/add', methods=['POST'])
+def add():
+    civilite = request.values.get('genre')
+    nom = request.values.get('nom')
+    prenom = request.values.get('prenom')
+    mail = request.values.get('mail')
+
+    adresse = request.values.get('adresse')
+    comp_adresse = request.values.get('compa')
+
+    code_postal = request.values.get('CP')
+    ville = request.values.get('ville')
+
+    pays = request.values.get('pays')
+
+    valeur_don = request.values.get('radio_montant')
+    type_don = request.values.get('paiement')
+
+    data.add_don(civilite, nom, prenom, mail, adresse, comp_adresse, code_postal, ville, pays, valeur_don, type_don)
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
---
+++ data.py
@@ -0,0 +1,29 @@
+import mysql.connector as mysqlpyth
+
+bdd = None
+cursor = None
+
+def connexion():
+    global bdd
+    global cursor
+
+    bdd = mysqlpyth.connect(user='admin_asso', password='mdp', host='localhost', port="8081", database='Test_don')
+    cursor = bdd.cursor()
+
+def deconnexion():
+    global bdd
+    global cursor
+
+    cursor.close()
+    bdd.close()
+
+def add_don(civilite, nom, prenom, mail, adresse, comp_adresse, code_postal, ville, pays, valeur_don, type_don):
+    global bdd
+    global cursor
+    
+    connexion()
+    query = "INSERT INTO dons(genre_don, nom_don, prenom_don, mail_don, adress_don, compa_don, cp_don, ville_don, pays_don, valeur_don, type_don) VALUES ('"+civilite+"','"+nom+"','"+prenom+"','"+mail+"','"+adresse+"','"+comp_adresse+"','"+code_postal+"','"+ville+"','"+pays+"','"+valeur_don+"','"+type_don+"');"
+
+    cursor.execute(query)
+    bdd.commit()
+    deconnexion()
