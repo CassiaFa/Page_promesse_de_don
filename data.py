9560245a7484334373c53f8bbe8882e644d6fd40
CassiaFa <fabio.gavancha-cassiano@isen-ouest.yncrea.fr>
2022-01-20 09:13:11 +0100

    Ajout d'utilisateurs
    Possibilité de se connecter au site pour accédé au tableau de bord, qui permet d'afficher les dons.

--- data.py
+++ data.py
@@ -21,24 +21,19 @@ def lire_dons():
     global cursor
 
     connexion()
-    query = "SELECT * FROM dons ORDER BY id_don DESC"
+    query = "SELECT id_don, date_don, genre_don, nom_don, prenom_don, valeur_don, type_don FROM dons ORDER BY id_don DESC"
     cursor.execute(query)
     dons = []
 
     for enregistrement in cursor :
         don = {}
         don['id'] = enregistrement[0]
-        don['genre'] = enregistrement[1]
-        don['nom'] = enregistrement[2]
-        don['prenom'] = enregistrement[3]
-        don['mail'] = enregistrement[4]
-        don['adresse'] = enregistrement[5]
-        don['compa'] = enregistrement[6]
-        don['CP'] = enregistrement[7]
-        don['ville'] = enregistrement[8]
-        don['pays'] = enregistrement[9]
-        don['valeur'] = enregistrement[10]
-        don['reglement'] = enregistrement[11]
+        don['date'] = enregistrement[1]
+        don['genre'] = enregistrement[2]
+        don['nom'] = enregistrement[3]
+        don['prenom'] = enregistrement[4]
+        don['valeur'] = enregistrement[5]
+        don['reglement'] = enregistrement[6]
         dons.append(don)
 
     query = "SELECT SUM(valeur_don) FROM Dons"
@@ -52,29 +47,29 @@ def lire_dons():
 
     return dons, cagnote
 
-def get_post(id):
-    global cursor
+# def get_post(id):
+#     global cursor
 
-    connexion()
-    query = "SELECT * FROM posts WHERE id=" + str(id)
-    cursor.execute(query)
-    post = {}
+#     connexion()
+#     query = "SELECT * FROM posts WHERE id=" + str(id)
+#     cursor.execute(query)
+#     post = {}
 
-    for enregistrement in cursor :
-        post['id'] = enregistrement[0]
-        post['created'] = enregistrement[1]
-        post['title'] = enregistrement[2]
-        post['content'] = enregistrement[3]
+#     for enregistrement in cursor :
+#         post['id'] = enregistrement[0]
+#         post['created'] = enregistrement[1]
+#         post['title'] = enregistrement[2]
+#         post['content'] = enregistrement[3]
 
-    deconnexion()
-    return post
+#     deconnexion()
+#     return post
 
 def add_don(civilite, nom, prenom, mail, adresse, comp_adresse, code_postal, ville, pays, valeur_don, type_don):
     global bdd
     global cursor
     
     connexion()
-    query = "INSERT INTO dons(genre_don, nom_don, prenom_don, mail_don, adress_don, compa_don, cp_don, ville_don, pays_don, valeur_don, type_don) VALUES ('"+civilite+"','"+nom+"','"+prenom+"','"+mail+"',"+adresse+","+comp_adresse+",'"+code_postal+"','"+ville+"','"+pays+"','"+valeur_don+"','"+type_don+"');"
+    query = "INSERT INTO dons(date_don, genre_don, nom_don, prenom_don, mail_don, adress_don, compa_don, cp_don, ville_don, pays_don, valeur_don, type_don) VALUES (CURRENT_DATE, '"+civilite+"','"+nom+"','"+prenom+"','"+mail+"',"+adresse+","+comp_adresse+",'"+code_postal+"','"+ville+"','"+pays+"','"+valeur_don+"','"+type_don+"');"
 
     cursor.execute(query)
     bdd.commit()
@@ -87,8 +82,11 @@ def check_user(login, mdp):
     query = "SELECT nom_libelle FROM libelles NATURAL JOIN utilisateurs WHERE login_utilisateur='"+login+"' AND mdp_utilisateur='"+mdp+"';"
 
     cursor.execute(query)
+    
+    droit = cursor.fetchone()
+    if droit != None:
+        droit = droit[0]
 
-    droit = cursor.fetchone()[0]
     deconnexion()
 
     return droit
